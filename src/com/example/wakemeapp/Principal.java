1599a3e489371c4b9401ae31d8529e61cd0334a4
Víctor Martínez <yazloz@gmail.com>
2014-03-13 17:53:52 +0100

    Confirmar 4
    asd

--- src/com/example/wakemeapp/Principal.java
+++ src/com/example/wakemeapp/Principal.java
@@ -35,21 +35,14 @@ public class Principal extends Activity {
 		return true;
 		
 	}
-<<<<<<< HEAD
 
-=======
-	
->>>>>>> 63c959b66febad17fec62742e05040a2541f1328
 	public void bucle(){
 		for(int i=0; i<100; i++)
 			System.out.println("Hola");
 	
 	}
 
-<<<<<<< HEAD
-=======
 
->>>>>>> 63c959b66febad17fec62742e05040a2541f1328
 	public void nuevafuncion (String uno){
 		int trescomacatorce =3;
 		float casicuatro= 3;
@@ -58,10 +51,7 @@ public class Principal extends Activity {
 	public boolean esPeritosPronto (Date fecha) {
 		return (true);
 	}
-<<<<<<< HEAD
 
-=======
->>>>>>> 63c959b66febad17fec62742e05040a2541f1328
 
 	public void mostrarFavoritos(){
 		NuevaAlarma a;
