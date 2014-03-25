7a7f356c313c9d9944a5f9fcd73f7c6e8bb84788
Víctor Martínez <yazloz@gmail.com>
2014-03-25 17:19:08 +0100

    Alarma Victor

--- src/clases/alarma.java
+++ src/clases/alarma.java
@@ -2,14 +2,15 @@ package clases;
 
 public class alarma {
 
-	public int id;
-	public String nombre;
-	public int distancia;
-	public boolean activa;
-	public String direccion;
-	public float latitud;
-	public float longitud;
-	public boolean favorito;
+	private int id;
+	private String nombre;
+	private int distancia;
+	private boolean activa;
+	private String direccion;
+	private float latitud;
+	private float longitud;
+	private boolean favorito;
+	private String destino;
 	
 	public alarma(){
 		
@@ -18,5 +19,87 @@ public class alarma {
 	public alarma(String nombre, int distancia){
 		this.nombre = nombre;
 		this.distancia = distancia;		
+	}
+	
+	public alarma(int id, String nombre, String destino, int distancia, boolean favorito, boolean activa)
+	{
+		this.id = id;
+		this.nombre = nombre;
+		this.destino = destino;
+		this.distancia = distancia;
+		this.favorito = favorito;
+		this.activa = activa; 
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getDistancia() {
+		return distancia;
+	}
+
+	public void setDistancia(int distancia) {
+		this.distancia = distancia;
+	}
+
+	public boolean isActiva() {
+		return activa;
+	}
+
+	public void setActiva(boolean activa) {
+		this.activa = activa;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public float getLatitud() {
+		return latitud;
+	}
+
+	public void setLatitud(float latitud) {
+		this.latitud = latitud;
+	}
+
+	public float getLongitud() {
+		return longitud;
+	}
+
+	public void setLongitud(float longitud) {
+		this.longitud = longitud;
+	}
+
+	public boolean isFavorito() {
+		return favorito;
+	}
+
+	public void setFavorito(boolean favorito) {
+		this.favorito = favorito;
+	}
+
+	public String getDestino() {
+		return destino;
+	}
+
+	public void setDestino(String destino) {
+		this.destino = destino;
 	}
 }
